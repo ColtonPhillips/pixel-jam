ed6342a07afa814adabb2629c93995e269505c27
Colton Phillips <coltonjphillips@gmail.com>
2014-03-14 01:00:21 -0700

    more filler and dinky python script

---
+++ gremlin/check.py
@@ -0,0 +1,20 @@
+import glob
+import os
+def main():
+	last_one = ""
+	for file in glob.glob("*.png"):
+		if last_one != "":
+			if file[1:3] == '00':
+				if last_one[1:3] != '99':
+					print(last_one)
+				last_one = file
+				continue
+
+			if int(file[1:3]) != int(last_one[1:3]) + 1:
+				print(last_one)
+				return
+
+		last_one = file
+
+if __name__ == "__main__":
+	main()
